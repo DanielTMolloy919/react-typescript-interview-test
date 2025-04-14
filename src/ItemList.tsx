bdcc5e6afe56c2ca8925bc63fbe5f58a4ef8fad9
Daniel Molloy <danieltmolloy919@gmail.com>
2025-04-14 12:20:00 +1000

    remove more comments

--- src/ItemList.tsx
+++ src/ItemList.tsx
@@ -1,5 +1,5 @@
 import { type Item } from "./data";
-// Simple component, assumes keys will be handled correctly in App.js map
+
 function ItemList({ items }: { items: Item[] }) {
   if (!items || items.length === 0) {
     return <p>No items match the filter.</p>;
@@ -7,7 +7,6 @@ function ItemList({ items }: { items: Item[] }) {
   return (
     <ul>
       {items.map((item) => (
-        // We'll add the key here now as it's not the bug focus
         <li key={item.id}>{item.name}</li>
       ))}
     </ul>
